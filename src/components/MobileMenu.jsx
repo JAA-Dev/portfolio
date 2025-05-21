b448985534dd25daf429c578d8b7f1731e0b3afb
JAA-Dev <jaadev19@gmail.com>
2025-05-21 12:48:22 -0700

    Adding HomePage/MobileMenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 export const MobileMenu = ({menuOpen, setMenuOpen}) => {
   return (
-    <div className={`fixed inset-0 bg-[#233831] z-40 flex flex-col
+    <div className={`fixed inset-0 bg-transparent backdrop-blur-lg z-40 flex flex-col
      items-center justify-center transition-all duration-300 ease-in-out
      ${menuOpen ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-full pointer-events-none"}`}>
-        <button onClick={() => setMenuOpen(false)} className='absolute top-6 right-6 text-white text-3xl
+        <button onClick={() => setMenuOpen(false)} className='absolute top-6 right-6 text-[#D4EDE4] text-3xl
          focus:outline-none cursor-pointer' aria-label='Close Menu'>&times;</button>
 
-        <a href="#home" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform
+        <a href="#home" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-[#D4EDE4] my-4 transform
              transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>Home</a>
-        <a href="#about" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-white my-4 transform
+        <a href="#about" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-[#D4EDE4] my-4 transform
              transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>About</a>
-        <a href="#project" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-white my-4 transform
+        <a href="#project" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-[#D4EDE4] my-4 transform
              transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>Project</a>
-        <a href="#footer" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-white my-4 transform
+        <a href="#footer" onClick={() => setMenuOpen(false)}  className={`text-2xl font-semibold text-[#D4EDE4] my-4 transform
              transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>Contact</a>
     </div>
   )
--- src/components/section/Home.jsx
+++ src/components/section/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-function Home() {
+export const Home = () => {
   return (
-    <div className='text-center' >Home</div>
+    <section id='home' className='bg-gradient min-h-screen flex items-center justify-center relative'>
+      <div className='flex flex-col items-center justify-center'>
+          <div className='text-center z-10 px-4'>
+            <h2 className='text-6xl md:text-7xl font-bold'>Hello! </h2>
+            <h2 className='text-6xl md:text-7xl font-bold'>I'm Augustine.</h2>
+            <h1 className='text-6xl md:text-7xl font-bold'>Front-end Developer.</h1>
+            <p className='mb-8 text-lg max-w-lg mx-auto'>I'm a passionate front-end 
+              developer dedicated to creating seamless, inituitive, and visually
+               engaging web expriences.
+            </p>
+          </div>
+          <div className='flex justify-center space-x-4'>
+            <a href='#' className='bg-black text-white py-3 px-6 rounded font-medium transition
+             relative overflow-hidden hover:-translate-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.4)'>Resume</a>
+            <a href='#' className='border border-[#233831] text-[#233831] py-3 px-6 rounded font-medium
+             transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59,130,246,0.2) hover:bg-[#233831]/50'>Contact Me</a>
+          </div>
+      </div>
+    </section>
   )
 }
 
